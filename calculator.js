0db6f3e8f218cb7943b5d41a4230e7f7769abf9e
AudunKodehode <audunkodehode@gmail.com>
2023-09-13 09:30:27 +0200

    allowed one single 0 digit, added code for checking animations

--- calculator.js
+++ calculator.js
@@ -4,53 +4,67 @@ let num1, num2, sum;
 let math = "none"
 function but1(){
     screenEl.textContent += 1
+    buttonPressed()
 }
 function but2(){
     screenEl.textContent += 2
+    buttonPressed()
 }
 function but3(){
     screenEl.textContent += 3
+    buttonPressed()
 }
 function but4(){
     screenEl.textContent += 4
+    buttonPressed()
 }
 function but5(){
     screenEl.textContent += 5
+    buttonPressed()
 }
 function but6(){
     screenEl.textContent += 6
+    buttonPressed()
 }
 function but7(){
     screenEl.textContent += 7
+    buttonPressed()
 }
 function but8(){
     screenEl.textContent += 8
+    buttonPressed()
 }
 function but9(){
     screenEl.textContent += 9
+    buttonPressed()
 }
 function but0(){
-    if (screenEl.textContent.substring(0,1) != 0){
+    if (screenEl.textContent.substring(0,1) != "0"){
     screenEl.textContent += 0
     }
+    buttonPressed()
 }
 function butdiv(){
     num1 = screenEl.textContent;
     math = "div";
     screenEl.textContent = ""
+    buttonPressed()
 }
 function butcomma(){
     screenEl.textContent += "."
+    buttonPressed()
 }
 function butmul(){
     num1 = parseFloat(screenEl.textContent);
     screenEl.textContent = ""
     math = "mul";
+    buttonPressed()
 }
 function butadd(){
     num1 = parseFloat(screenEl.textContent);
     screenEl.textContent = ""
     math = "add";
+    buttonPressed()
 }
 function butcomma(){
     let checkString = screenEl.textContent;
@@ -58,6 +72,7 @@ function butcomma(){
     if (checkString.includes(".") == false){
         screenEl.textContent += "."
     }
+    buttonPressed()
 }
 function butequals(){
 if (math == "none"){
@@ -82,11 +97,13 @@ screenEl.textContent = sum
 }
 
 
+buttonPressed()
 }
 function butsub(){
     num1 = screenEl.textContent;
     math = "sub";
     screenEl.textContent = ""
+    buttonPressed()
 }
 function butclear(){
     num1 = 0;
@@ -94,4 +111,33 @@ function butclear(){
     sum = 0;
     screenEl.textContent = "";
     math = "none"
+    buttonPressed()
+}
+let animTrigger1 = "1337"
+let animTrigger2 = "69"
+let animTrigger3 = "420"
+let animTrigger4 = "5318008"
+
+function buttonPressed(){
+    let result = screenEl.textContent.trim();
+    if (result == animTrigger1){
+        ///leet
+}
+
+
+if (result == animTrigger2){
+    ///69
+}
+
+if (result == animTrigger3){
+    ///420
+}
+
+if (result == animTrigger4){
+    ///5318008
+}
+
+
+
+
 }
